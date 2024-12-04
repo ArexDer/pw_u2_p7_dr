a5be21d6ffdd11d77d9478472a0d43359815fd70
Diego Rivas <darivas@uce.edu.ec>
2024-12-04 21:16:30 +0000

    tarea de Reactjs

---
+++ js/appReact.js
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div>
+            <h1>Hola Mundo ReactJS</h1>
+            <h2>REACTJS</h2>
+        </div>
+    );
+}
+
+// Renderizar el componente en el DOM
+ReactDOM.render(<App />, document.getElementById('root'));
