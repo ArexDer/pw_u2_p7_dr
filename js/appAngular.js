8698e13c8c792d1d70b382466ae85481041a4dd7
Diego Rivas <darivas@uce.edu.ec>
2024-12-04 15:24:54 +0000

    Reactjs y Angularjs Hola mundo

---
+++ js/appAngular.js
@@ -0,0 +1,21 @@
+(function (ng) {
+    const { NgModule, Component, enableProdMode } = ng.core;
+    const { BrowserModule } = ng.platformBrowser;
+
+    enableProdMode();
+
+    @Component({
+        selector: 'app-root',
+        template: '<h1>Hola Mundo</h1>'
+    })
+    class AppComponent { }
+
+    @NgModule({
+        declarations: [AppComponent],
+        imports: [BrowserModule],
+        bootstrap: [AppComponent]
+    })
+    class AppModule { }
+
+    ng.platformBrowserDynamic.platformBrowserDynamic().bootstrapModule(AppModule);
+})(window.ng);
